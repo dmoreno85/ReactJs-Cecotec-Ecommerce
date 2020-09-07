e5c297f1d16446f050053fdfbe8cf5bc57dfce79
David <dmoreno788@gmail.com>
2020-09-07 16:21:23 +0200

    Componente Search funcionando

---
+++ src/Components/Search/Search.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from "prop-types";
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { search: "" };
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          placeholder="Buscar.."
+          className="search"
+          name="search"
+          onChange={(event) => this.props.handleSearch(event)}
+          value={this.props.search}
+        />
+      </div>
+    );
+  }
+}
+Search.propTypes = { handleSearch: PropTypes.func.isRequired };
+
+export default Search;
